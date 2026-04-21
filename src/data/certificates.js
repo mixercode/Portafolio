fc78c84ec1bfd5bc8e1253c310ce8f3c618d70ce
Miguel Flores <jmiguel.flores121@gmail.com>
2026-04-21 12:55:31 -0600

    `Removed thumbnails and updated certificates data`

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,114 +1,115 @@
-//TODO: agregar nombre del curso en el título de cada certificado
-
 export const CERTIFICATES_DATA = [
   {
     id: 1,
     title: "CódigoFest 2025 powered by Microsoft",
     pdfUrl:
       "/certificados/Certificado - CÓDIGOFEST 2025 powered by Microsoft.pdf",
-    previewUrl: "/thumbnails/codigo facilito-codigofest.png",
+    previewUrl: "/thumbnails/certificates/codigo facilito-codigofest.png",
   },
   {
     id: 2,
     title: "Curso a fondo de GitHub",
     pdfUrl: "/certificados/Certificado - Curso a fondo de GitHub.pdf",
-    previewUrl: "/thumbnails/codigo facilito-github.png",
+    previewUrl: "/thumbnails/certificates/codigo facilito-github.png",
   },
   {
     id: 3,
     title: "Curso de Introducción a DevOps: Bases y Conceptos",
     pdfUrl:
       "/certificados/Certificado - Curso de Introducción a DevOps_ Bases y Conceptos.pdf",
-    previewUrl: "/thumbnails/codigo facilito-devops.png",
+    previewUrl: "/thumbnails/certificates/codigo facilito-devops.png",
   },
   {
     id: 4,
     title: "Curso de Visual Studio Code y GitHub Copilot",
     pdfUrl:
       "/certificados/Certificado - Curso de Visual Studio Code y GitHub Copilot.pdf",
-    previewUrl: "/thumbnails/codigo facilito-vsc copilot.png",
+    previewUrl: "/thumbnails/certificates/codigo facilito-vsc copilot.png",
   },
   {
     id: 5,
     title: "Curso profesional de Git",
     pdfUrl: "/certificados/Certificado - Curso profesional de Git.pdf",
-    previewUrl: "/thumbnails/codigo facilito-git.png",
+    previewUrl: "/thumbnails/certificates/codigo facilito-git.png",
   },
   {
     id: 6,
     title: "Curso de metodologías para solucion de problemas",
     pdfUrl:
       "/certificados/Certificado_Curso_de_metodologías_para_solución_de_problemas.pdf",
-    previewUrl: "/thumbnails/codigo facilito-metodologia para solucion.png",
+    previewUrl:
+      "/thumbnails/certificates/codigo facilito-metodologia para solucion.png",
   },
   {
     id: 7,
     title: "Certificado de iniciación al desarrollo con IA",
     pdfUrl: "/certificados/Certificado-Jose-Miguel-Flores-Flores-e6lkmw92.pdf",
-    previewUrl: "/thumbnails/moure-de 0 a produccion.png",
+    previewUrl: "/thumbnails/certificates/moure-de 0 a produccion.png",
   },
   {
     id: 8,
     title: "Certificado Programación Web",
     pdfUrl: "/certificados/Certificado-Programación-Web-EducaciónIT.pdf",
-    previewUrl: "/thumbnails/education-programacion web.png",
+    previewUrl: "/thumbnails/certificates/education-programacion web.png",
   },
   {
     id: 9,
     title: "Certificado Undesrstanding Prompt Engineering",
     pdfUrl: "/certificados/certificate (1).pdf",
-    previewUrl: "/thumbnails/datacamp-understanding prompt.png",
+    previewUrl: "/thumbnails/certificates/datacamp-understanding prompt.png",
   },
   {
     id: 10,
     title: "Certificado AWS Concepts",
     pdfUrl: "/certificados/certificate (2).pdf",
-    previewUrl: "/thumbnails/datacamp-aws concepts.png",
+    previewUrl: "/thumbnails/certificates/datacamp-aws concepts.png",
   },
   {
     id: 11,
     title: "Concstancia Congreso internacional - CONACIC 2019",
     pdfUrl: "/certificados/Constancia_CONACIC_2019_Rx31816_Tx_1575751688.pdf",
-    previewUrl: "/thumbnails/Buap-04 19.png",
+    previewUrl: "/thumbnails/certificates/Buap-04 19.png",
   },
   {
     id: 12,
     title: "Constancia Congreso nacional - CONACIC 2022",
     pdfUrl:
       "/certificados/Constancia_CONACIC_2020_Nacional_Rx_2500_Tx_8731.pdf",
-    previewUrl: "/thumbnails/Buap-07 22.png",
+    previewUrl: "/thumbnails/certificates/Buap-07 22.png",
   },
   {
     id: 13,
     title: "Constancia Congreso internacional - CONACIC 2020",
     pdfUrl:
       "/certificados/Constancia_CONACIC_2020_Nacional_Rx_3408_Tx_9141.pdf",
-    previewUrl: "/thumbnails/Buap-13 20.png",
+    previewUrl: "/thumbnails/certificates/Buap-13 20.png",
   },
   {
     id: 14,
     title:
       "Constancia taller de deteccion de objetos mediante vision por computadora",
     pdfUrl: "/certificados/Constancia_CONACIC_2021_Taller_Rx_2460_Tx_5869.pdf",
-    previewUrl: "/thumbnails/Buap-07 22.png",
+    previewUrl: "/thumbnails/certificates/Buap-07 22.png",
   },
   {
     id: 15,
     title: "Constancia Congreso internacional - CONACIC 2022",
     pdfUrl:
       "/certificados/Constancia_CONACIC_2022_Internacional_Rx_2511_Tx_5438.pdf",
-    previewUrl: "/thumbnails/Buap-06 22.png",
+    previewUrl: "/thumbnails/certificates/Buap-06 22.png",
   },
   {
     id: 16,
     title: "Certificado desarrollador de JavaScript (Node.js)",
     pdfUrl: "/certificados/Desarrollador-de-javascript(nodejs).pdf",
-    previewUrl: "/thumbnails/carlos slim-desarrollador de javascript.png",
+    previewUrl:
+      "/thumbnails/certificates/carlos slim-desarrollador de javascript.png",
   },
   {
     id: 17,
     title: "Curso de iniciación al desarrollo con IA",
     pdfUrl: "/certificados/José Miguel _jmiguel.flores121@gmail.com.pdf",
-    previewUrl: "/thumbnails/moure-iniciacion al desarrolo con ia.png",
+    previewUrl:
+      "/thumbnails/certificates/moure-iniciacion al desarrolo con ia.png",
   },
 ];
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,17 +3,27 @@ import WindowsCard from "../components/WindowsCard";
 
 export default function ProjectsPage() {
   return (
-    <NavigationCard title={"Projects"}>
-      <div className="flex flex-row gap-10">
-        <p className="w-1/2 text-left">
-          I'm Miguel, a passionate developer with a love for creating dynamic
-          and responsive web applications. With a strong foundation in
-          JavaScript and React, I enjoy bringing ideas to life through code.
-        </p>
-        <WindowsCard>
-          <p>asdasdasdsdsdasaasdsdsdsdsaDSdasdasdd</p>
-        </WindowsCard>
+    <>
+      <NavigationCard title={"Projects"}>
+        <div className="flex flex-row gap-10">
+          <p className="w-1/2 text-left">
+            I'm Miguel, a passionate developer with a love for creating dynamic
+            and responsive web applications. With a strong foundation in
+            JavaScript and React, I enjoy bringing ideas to life through code.
+          </p>
+          <WindowsCard>
+            <p>asdasdasdsdsdasaasdsdsdsdsaDSdasdasdd</p>
+          </WindowsCard>
+        </div>
+      </NavigationCard>
+
+      <div className="flex flex-row items-center gap-4 mt-10 mb-6">
+        <div
+          aria-hidden="true"
+          className="w-5 h-5 rounded-full border-10 border-[#164ae8] shadow-[0_0_8px_rgba(22,74,232,0.5)]"
+        />
+        <h2 className="text-3xl font-bold text-gray-100 ">Restaurante</h2>
       </div>
-    </NavigationCard>
+    </>
   );
 }
