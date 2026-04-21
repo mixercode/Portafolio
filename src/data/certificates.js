c48f34afd2fc4039be9ce2b2f3f353ff57621e42
Miguel Flores <jmiguel.flores121@gmail.com>
2026-04-20 20:06:54 -0600

    `Removed certificates and thumbnails, updated certificates.js file`

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -3,123 +3,111 @@
 export const CERTIFICATES_DATA = [
   {
     id: 1,
-    title: "Certificado de Reactasdasdasdasdasdasdasd",
+    title: "CódigoFest 2025 powered by Microsoft",
     pdfUrl:
       "/certificados/Certificado - CÓDIGOFEST 2025 powered by Microsoft.pdf",
     previewUrl: "/thumbnails/codigo facilito-codigofest.png",
   },
   {
     id: 2,
-    title: "Certificado de React",
+    title: "Curso a fondo de GitHub",
     pdfUrl: "/certificados/Certificado - Curso a fondo de GitHub.pdf",
     previewUrl: "/thumbnails/codigo facilito-github.png",
   },
   {
     id: 3,
-    title: "Certificado de React",
+    title: "Curso de Introducción a DevOps: Bases y Conceptos",
     pdfUrl:
       "/certificados/Certificado - Curso de Introducción a DevOps_ Bases y Conceptos.pdf",
     previewUrl: "/thumbnails/codigo facilito-devops.png",
   },
   {
     id: 4,
-    title: "Certificado de React",
+    title: "Curso de Visual Studio Code y GitHub Copilot",
     pdfUrl:
       "/certificados/Certificado - Curso de Visual Studio Code y GitHub Copilot.pdf",
     previewUrl: "/thumbnails/codigo facilito-vsc copilot.png",
   },
   {
     id: 5,
-    title: "Certificado de React",
+    title: "Curso profesional de Git",
     pdfUrl: "/certificados/Certificado - Curso profesional de Git.pdf",
     previewUrl: "/thumbnails/codigo facilito-git.png",
   },
   {
     id: 6,
-    title: "Certificado de React",
-    pdfUrl: "/Certificado_Curso_de_metodologías_para_solución_de_problemas.pdf",
+    title: "Curso de metodologías para solucion de problemas",
+    pdfUrl:
+      "/certificados/Certificado_Curso_de_metodologías_para_solución_de_problemas.pdf",
     previewUrl: "/thumbnails/codigo facilito-metodologia para solucion.png",
   },
   {
     id: 7,
-    title: "Certificado de React",
+    title: "Certificado de iniciación al desarrollo con IA",
     pdfUrl: "/certificados/Certificado-Jose-Miguel-Flores-Flores-e6lkmw92.pdf",
     previewUrl: "/thumbnails/moure-de 0 a produccion.png",
   },
   {
     id: 8,
-    title: "Certificado de React",
-    pdfUrl: "/Certificado-Programación-Web-EducaciónIT.pdf",
+    title: "Certificado Programación Web",
+    pdfUrl: "/certificados/Certificado-Programación-Web-EducaciónIT.pdf",
     previewUrl: "/thumbnails/education-programacion web.png",
   },
   {
     id: 9,
-    title: "Certificado de React",
+    title: "Certificado Undesrstanding Prompt Engineering",
     pdfUrl: "/certificados/certificate (1).pdf",
     previewUrl: "/thumbnails/datacamp-understanding prompt.png",
   },
   {
     id: 10,
-    title: "Certificado de React",
+    title: "Certificado AWS Concepts",
     pdfUrl: "/certificados/certificate (2).pdf",
     previewUrl: "/thumbnails/datacamp-aws concepts.png",
   },
   {
     id: 11,
-    title: "Certificado de React",
-    pdfUrl: "/Constancia_CONACIC_2019_Rx31816_Tx_1575751688.pdf",
+    title: "Concstancia Congreso internacional - CONACIC 2019",
+    pdfUrl: "/certificados/Constancia_CONACIC_2019_Rx31816_Tx_1575751688.pdf",
     previewUrl: "/thumbnails/Buap-04 19.png",
   },
   {
     id: 12,
-    title: "Certificado de React",
+    title: "Constancia Congreso nacional - CONACIC 2022",
     pdfUrl:
       "/certificados/Constancia_CONACIC_2020_Nacional_Rx_2500_Tx_8731.pdf",
     previewUrl: "/thumbnails/Buap-07 22.png",
   },
   {
     id: 13,
-    title: "Certificado de React",
+    title: "Constancia Congreso internacional - CONACIC 2020",
     pdfUrl:
       "/certificados/Constancia_CONACIC_2020_Nacional_Rx_3408_Tx_9141.pdf",
     previewUrl: "/thumbnails/Buap-13 20.png",
   },
   {
     id: 14,
-    title: "Certificado de React",
+    title:
+      "Constancia taller de deteccion de objetos mediante vision por computadora",
     pdfUrl: "/certificados/Constancia_CONACIC_2021_Taller_Rx_2460_Tx_5869.pdf",
     previewUrl: "/thumbnails/Buap-07 22.png",
   },
   {
     id: 15,
-    title: "Certificado de React",
+    title: "Constancia Congreso internacional - CONACIC 2022",
     pdfUrl:
       "/certificados/Constancia_CONACIC_2022_Internacional_Rx_2511_Tx_5438.pdf",
     previewUrl: "/thumbnails/Buap-06 22.png",
   },
   {
     id: 16,
-    title: "Certificado de React",
+    title: "Certificado desarrollador de JavaScript (Node.js)",
     pdfUrl: "/certificados/Desarrollador-de-javascript(nodejs).pdf",
     previewUrl: "/thumbnails/carlos slim-desarrollador de javascript.png",
   },
   {
     id: 17,
-    title: "Certificado de React",
-    pdfUrl:
-      "/certificados/Google_Interland_José_Miguel_Flores_Flores_Certificate_of_Alertness.pdf",
-    previewUrl: "/thumbnails/google-internet alertness.png",
-  },
-  {
-    id: 18,
-    title: "Certificado de React",
-    pdfUrl:
-      "/certificados/Google_Interland_José_Miguel_Flores_Flores_Certificate_of_Smartness.pdf",
-    previewUrl: "/thumbnails/google-internet smartness.png",
-  },
-  {
-    id: 19,
-    title: "Certificado de React",
+    title: "Curso de iniciación al desarrollo con IA",
     pdfUrl: "/certificados/José Miguel _jmiguel.flores121@gmail.com.pdf",
     previewUrl: "/thumbnails/moure-iniciacion al desarrolo con ia.png",
   },
